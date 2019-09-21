3a074a14a20c4707743c01a7a498393abc7c0775
rarun321 <43283635+rarun321@users.noreply.github.com>
2019-09-21 12:02:15 -0700

    Added UI

---
+++ ChromAR/ColorScanner.swift
@@ -0,0 +1,36 @@
+//
+//  ColorScanner.swift
+//  ChromAR
+//
+//  Created by Rithvik Arun on 9/21/19.
+//  Copyright © 2019 Rithvik Arun. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+import UIKit
+
+//ColorScanner is the rectangle in the middle of the application that detects color
+class ColorScanner{
+    
+    let rectanglePositionX : CGFloat
+    let rectanglePositionY : CGFloat
+    let rectangleWidth : CGFloat
+    let rectangleHeight : CGFloat
+    
+    init(rectanglePositionX : CGFloat, rectanglePositionY: CGFloat, rectangleWidth : CGFloat, rectangleHeight : CGFloat ) {
+        self.rectanglePositionX = rectanglePositionX
+        self.rectanglePositionY = rectanglePositionY
+        self.rectangleWidth = rectangleWidth
+        self.rectangleHeight = rectangleHeight
+    }
+    
+    //Creates CGRectangle For Scanning Color
+    func CreateColorScanner() -> UIView{
+        let colorScanner = CGRect(x: rectanglePositionX, y: rectanglePositionY, width: rectangleWidth, height: rectangleHeight)
+        let colorScannerView = UIView(frame: colorScanner)
+        colorScannerView.backgroundColor = UIColor.clear
+        return colorScannerView
+    }
+}
+
